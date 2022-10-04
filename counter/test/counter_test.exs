684ac89b003e5dae9cc0cc0cf59bee9f4296ddea
Rafi Dudekula <rafi.dudekula@gmail.com>
2022-10-04 18:02:19 -0500

    OTP counter sample

--- counter/test/counter_test.exs
+++ counter/test/counter_test.exs
@@ -2,7 +2,17 @@ defmodule CounterTest do
   use ExUnit.Case
   doctest Counter
 
-  test "greets the world" do
-    assert Counter.hello() == :world
+  test "inc increments an integer value" do
+    assert Counter.Core.inc(1) == 2
+  end
+
+  test "use Counter through API" do
+    pid = Counter.start(0)
+    assert Counter.state(pid) == 0
+
+    Counter.tick(pid)
+    Counter.tick(pid)
+    Counter.tick(pid)
+    assert Counter.state(pid) == 3
   end
 end
