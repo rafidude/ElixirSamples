3ca6c4369171ca461027ca0801e6def6deba55cd
Rafi Dudekula <rafi.dudekula@gmail.com>
2022-10-26 06:53:11 -0500

    added string protocol for PID

--- spawn/sp1.exs
+++ spawn/sp1.exs
@@ -1,9 +1,21 @@
+# String.Chars Protocol for Elixir PIDs
+# https://devbrett.com/2018/08/implement-string-protocol-elixir-pid.html
+
+defimpl String.Chars, for: PID do
+  def to_string(pid) do
+    # info = Process.info pid
+    # name = info[:registered_name]
+
+    # "#{name}-#{inspect pid}"
+    "#{inspect pid}"
+  end
+end
+
 defmodule Sp1 do
   def greet do
     receive do
       {sender, msg} ->
-        IO.inspect self()
-        IO.puts "Received message #{msg}"
+        IO.puts "#{self()}: Received message #{msg}"
         send sender, { :ok, "Hello, #{msg}" }
     end
     greet()
@@ -13,20 +25,18 @@ end
 # here's a client
 pid = spawn(Sp1, :greet, [])
 msg1 = "World!"
-IO.puts "Sending message1 #{msg1}"
+IO.puts "#{self()}: Sending message1 #{msg1}"
 send pid, {self(), msg1}
 receive do
   {:ok, message} ->
-    IO.inspect self()
     IO.puts "Final message1: #{message}"
 end
 
 msg2 = "Kermit!"
-IO.puts "Sending message2 #{msg2}"
+IO.puts "#{self()}: Sending message2 #{msg2}"
 send pid, {self(), msg2}
 receive do
   {:ok, message} ->
-    IO.inspect self()
     IO.puts "Final message2: #{message}"
   after 500 ->
     IO.puts "The greeter has gone away"
