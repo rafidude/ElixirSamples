7d9d0312ddc50e4fe9ed7d8f9609a4c3d0a0e49a
Rafi Dudekula <rafi.dudekula@gmail.com>
2022-10-26 07:27:35 -0500

    added spawn_monitor

--- spawn/link.exs
+++ spawn/link.exs
@@ -1,6 +1,8 @@
 # spawn only: Nothing happened as far as I am concerned
 # spawn_link only: ** (EXIT from #PID<0.95.0>) :boom
-# process_flag: MESSAGE RECEIVED: {:EXIT, #PID<0.102.0>, :boom}
+# Process.flag: MESSAGE RECEIVED: {:EXIT, #PID<0.102.0>, :boom}
+# spawn_monitor: MESSAGE RECEIVED: {:DOWN, #Reference<0.1361150353.2291138562.165190>, :process, #PID<0.102.0>, :boom}
+
 defmodule Link3 do
   import :timer, only: [ sleep: 1 ]
 
@@ -11,7 +13,8 @@ defmodule Link3 do
   def run do
     Process.flag(:trap_exit, true)
     # spawn(Link3, :sad_function, [])
-    spawn_link(Link3, :sad_function, [])
+    # spawn_link(Link3, :sad_function, [])
+    spawn_monitor(Link3, :sad_function, [])
     receive do
       msg ->
         IO.puts "MESSAGE RECEIVED: #{inspect msg}"
