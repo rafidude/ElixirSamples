021a5ddcca1c63e3a921f0ba546e4ca5e3e4c545
Rafi Dudekula <rafi.dudekula@gmail.com>
2022-10-26 06:46:43 -0500

    OTP processes simple spawn

--- spawn/sp1.exs
+++ spawn/sp1.exs
@@ -2,6 +2,8 @@ defmodule Sp1 do
   def greet do
     receive do
       {sender, msg} ->
+        IO.inspect self()
+        IO.puts "Received message #{msg}"
         send sender, { :ok, "Hello, #{msg}" }
     end
     greet()
@@ -10,16 +12,22 @@ end
 
 # here's a client
 pid = spawn(Sp1, :greet, [])
-send pid, {self(), "World!"}
+msg1 = "World!"
+IO.puts "Sending message1 #{msg1}"
+send pid, {self(), msg1}
 receive do
   {:ok, message} ->
-    IO.puts message
+    IO.inspect self()
+    IO.puts "Final message1: #{message}"
 end
 
-send pid, {self(), "Kermit!"}
+msg2 = "Kermit!"
+IO.puts "Sending message2 #{msg2}"
+send pid, {self(), msg2}
 receive do
   {:ok, message} ->
-    IO.puts message
+    IO.inspect self()
+    IO.puts "Final message2: #{message}"
   after 500 ->
     IO.puts "The greeter has gone away"
 end
