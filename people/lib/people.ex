00b390e4e14c5cef835a3d451fc6eb9ae580e8d9
Rafi Dudekula <rafi.dudekula@gmail.com>
2022-11-05 09:23:55 -0500

    complex maps based DS for people context

---
+++ people/lib/people.ex
@@ -0,0 +1,36 @@
+defmodule People do
+  defstruct auto_id: 1, data: %{}
+  def new() do
+    %People{}
+  end
+
+  def create(people, person) do
+    person = Map.put(person, :id, people.auto_id)
+    new_data = Map.put(people.data, people.auto_id, person)
+    %People{people | auto_id: people.auto_id + 1, data: new_data }
+  end
+
+  def read(people, age) do
+    people.data
+    |> Enum.filter(fn {_, v} -> v.age == age end)
+    |> Enum.map(fn {_, v} -> v end)
+  end
+
+  def delete(people, id) do
+    %People{ people | data: Map.delete(people.data, id)}
+  end
+
+  def update(people, %{} = person) do
+    update(people, person.id, person)
+  end
+
+  def update(people, id, person) do
+    case Map.fetch(people.data, id) do
+      :error ->
+        people
+      {:ok, _} ->
+        new_data = Map.put(people.data, person.id, person)
+        %People{people | data: new_data }
+    end
+  end
+end
