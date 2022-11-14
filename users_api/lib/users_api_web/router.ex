352f565d19dc227ccf0ac7c9850522cb43d4dc69
Rafi Dudekula <rafi.dudekula@gmail.com>
2022-11-14 06:08:34 -0600

    users_api using Phoenix framework

---
+++ users_api/lib/users_api_web/router.ex
@@ -0,0 +1,13 @@
+defmodule UsersApiWeb.Router do
+  use UsersApiWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", UsersApiWeb do
+    pipe_through :api
+
+    resources "/users", UserController, except: [:new, :edit]
+  end
+end
