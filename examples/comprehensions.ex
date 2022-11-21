c4d10aa4857e0daddb2c7885072ce05341002623
Rafi Dudekula <rafi.dudekula@gmail.com>
2022-11-21 14:32:59 -0600

    Added format change comprehension

--- examples/comprehensions.ex
+++ examples/comprehensions.ex
@@ -13,4 +13,17 @@ defmodule Comp do
   def sqrt_map(c) do
     for e <- c, into: %{}, do: {e, :math.sqrt(e)}
   end
+
+  @tax 0.13
+  def format_employee_data(collection) do
+    for {name, salary} <- collection, into: Map.new,
+    do: {format_name(name), salary - salary * @tax}
+  end
+
+  defp format_name(name) do
+    name |>
+    String.downcase |>
+    String.to_atom
+  end
+  # Comp.format_employee_data( %{"Joe" => 50, "Bill" => 40, "Alice" => 45, "Jim" => 30} ) |> IO.inspect
 end
